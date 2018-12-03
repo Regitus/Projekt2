afced8cc75dc94b31291b2784aa390fcddf3e7a2
Regitus <33863615+Regitus@users.noreply.github.com>
2018-12-03 13:12:25 +0100

    FirstCommit
    Erster Commit um ein Master zu haben

---
+++ src/projekt/main/MainDing.java
@@ -0,0 +1,12 @@
+package projekt.main;
+
+import de.thm.oop.chat.base.server.BasicTHMChatServer;
+
+public class MainDing {
+	
+	public static void main(String[] args)
+	{
+		BasicTHMChatServer test = new BasicTHMChatServer();
+	}
+
+}
