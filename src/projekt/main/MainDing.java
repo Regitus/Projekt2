827ec24427a294cea03e764f1e1301f8ddf9c392
Regitus <33863615+Regitus@users.noreply.github.com>
2018-12-03 15:20:24 +0100

    Update MainDing.java
    Leeren der Main für besseres

--- src/projekt/main/MainDing.java
+++ src/projekt/main/MainDing.java
@@ -18,45 +18,6 @@ public class MainDing {
 		System.out.println("Pass");
 		pass = in.next();
 		
-		String[] arSTring;
-		BasicTHMChatServer test = new BasicTHMChatServer();
-		try {
-			arSTring = test.getUsers(user, pass);
-			for (String tmpString : arSTring)//(int i = 0; i<arSTring.length; i++)
-			{
-				System.out.println(tmpString);
-			}
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			arSTring = test.getMostRecentMessages(user, pass);
-			for (int i = 0; i<arSTring.length; i++)
-			{
-				System.out.println(arSTring[i]);
-			}
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		/*try {
-			test.sendTextMessage(user, pass, "TimSchoett", "Hi :D");
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
 	}
 
 }
