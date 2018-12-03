5786ac5ec74cd2a46bdeb67e776e4be24f4f2422
Regitus <33863615+Regitus@users.noreply.github.com>
2018-12-03 13:18:19 +0100

    Update MainDing.java
    getUser ausprobiert

--- src/projekt/main/MainDing.java
+++ src/projekt/main/MainDing.java
@@ -1,12 +1,31 @@
 package projekt.main;
 
+import java.io.IOException;
+
 import de.thm.oop.chat.base.server.BasicTHMChatServer;
 
 public class MainDing {
 	
 	public static void main(String[] args)
 	{
+		
+		String[] arSTring;
 		BasicTHMChatServer test = new BasicTHMChatServer();
+		try {
+			arSTring = test.getUsers("Rene_Terry", "0000");
+			for (int i = 0; i<arSTring.length; i++)
+			{
+				System.out.println(arSTring[i]);
+			}
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
