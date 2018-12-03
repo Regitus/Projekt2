2abbc9803cdfcfbfd1d162b3c759040370245dae
Regitus <33863615+Regitus@users.noreply.github.com>
2018-12-03 15:18:12 +0100

    Changes for testing

--- src/projekt/main/MainDing.java
+++ src/projekt/main/MainDing.java
@@ -1,6 +1,7 @@
 package projekt.main;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import de.thm.oop.chat.base.server.BasicTHMChatServer;
 
@@ -10,13 +11,20 @@ public class MainDing {
 	{
 		String pass = "0000";
 		String user = "Bla";
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("User");
+		user = in.next();
+		System.out.println("Pass");
+		pass = in.next();
+		
 		String[] arSTring;
 		BasicTHMChatServer test = new BasicTHMChatServer();
 		try {
 			arSTring = test.getUsers(user, pass);
-			for (int i = 0; i<arSTring.length; i++)
+			for (String tmpString : arSTring)//(int i = 0; i<arSTring.length; i++)
 			{
-				System.out.println(arSTring[i]);
+				System.out.println(tmpString);
 			}
 		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
@@ -38,6 +46,16 @@ public class MainDing {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		/*try {
+			test.sendTextMessage(user, pass, "TimSchoett", "Hi :D");
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 		
 	}
 
