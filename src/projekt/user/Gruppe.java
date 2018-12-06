02ee904aa5aa8eefa01290b393675d2a0ae4f097
Regitus <33863615+Regitus@users.noreply.github.com>
2018-12-06 09:52:38 +0100

    Update Gruppe.java

--- src/projekt/user/Gruppe.java
+++ src/projekt/user/Gruppe.java
@@ -1,11 +1,37 @@
 package projekt.user;
 
 public class Gruppe extends Personen {
+	
+	private String gruppenName;
+	
+	/**
+	 * @return the gruppenName
+	 */
+	
+	public Gruppe(String name, String[] liste)
+	{
+		listePersonen = liste;
+		setGruppenName(name);
+	}
+	
+	
+	public String getGruppenName() {
+		return gruppenName;
+	}
+
+	/**
+	 * @param gruppenName the gruppenName to set
+	 */
+	public void setGruppenName(String gruppenName) {
+		this.gruppenName = gruppenName;
+	}
+
+
 
 	@Override
 	public String[] getListe() {
 		// TODO Auto-generated method stub
-		return null;
+		return listePersonen;
 	}
 
 }
